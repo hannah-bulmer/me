c5fbd518f02b36221b0bdb3561e0eb04527fc549
Hannah <hannah@MacBook-Pro-4.local>
2019-02-10 21:29:04 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './Button.js';
+import Header from './Header.js';
+import ScrollableAnchor, { configureAnchors } from 'react-scrollable-anchor'
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    configureAnchors({offset: -10, scrollDuration: 200})
+  }
+
   render() {
     return (
+      <div id='root'>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Header />
+        <h1>HELLO</h1>
+        <h2>This is my website</h2>
+        <Button name="colour"/>
+        <Button name="essay"/>
+        </div>
+        <p id="space"></p>
+      <ScrollableAnchor id="about">
+      <h3 id="App-about">Hello this is a WIP. This is my About me page!
+      </h3>
+      </ScrollableAnchor>
+      <p id="space"></p>
       </div>
     );
   }

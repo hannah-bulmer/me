1620d5247452051751481bb1b7928ec03f13aaf5
Hannah <hannah@MacBook-Pro-4.local>
2019-05-15 16:31:07 -0400

    Added call for weather

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,11 +2,40 @@ import React from 'react';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 
 class DataDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      progress: 60,
+    }
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(), 1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+
+  tick = () => {
+    this.setState({ progress: this.state.progress == 100 ? 0 : this.state.progress + 1})
+  }
+
   render() {
     return (
       <div>
         <h1>Experience</h1>
-        <ProgressBar now={60} style={{width: '100px'}}/>
+        <div className="rightCol background">
+          <ProgressBar
+            striped
+            now={this.state.progress}
+            style={{width: '300px'}}
+            label="Coming soon"
+          />
+        </div>
       </div>)
   }
 }
--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -2,16 +2,78 @@ import React from 'react';
 import WebsiteLink from './WebsiteLink';
 
 class FrontPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weather: '',
+      showDropdown: false,
+    }
+    this.myDropdown = React.createRef();
+  }
+
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleClick, false);
+    this.getWeather(6176823);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClick, false);
+  }
+
+  handleClick = (e) => {
+    console.log(e.target);
+    if (!this.myDropdown.contains(e.target)) this.openDropdown();
+  }
+
+  getWeather = async (id) => {
+    const Api_Key = '859499bfed4c14594bd97b7545189cfb';
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${Api_Key}`);
+      const response = await api_call.json();
+      this.setState({ weather: response })
+    console.log("Getting response");
+  }
+
+  getNewWeather = (e) => {
+    this.getWeather(e.target.value);
+  }
+
+  openDropdown = () => {
+    this.setState({ showDropdown: !this.state.showDropdown })
+  }
+
   renderLinks() {
     return (
       this.props.links.map(link => <WebsiteLink link={link.link} image={link.image} />)
     )
   }
+  
+  renderSearch(name) {
+    const dropdownClass = "dropdown-content " + (this.state.showDropdown && "open");
+    return (
+      <div class="dropdown" ref={myDropdown => this.myDropdown = myDropdown}>
+        <button class="dropbtn" onClick={this.openDropdown}>{name}</button>
+        <div class={dropdownClass}>
+          <button value={6167865} onClick={this.getNewWeather}>Toronto</button>
+          <button value={6176823} onClick={this.getNewWeather}>Waterloo</button>
+          <button value={5128638} onClick={this.getNewWeather}>New York</button>
+      </div>
+    </div>
+    )
+  }
 
   render() {
+    let weather = '';
+    let weatherType = '';
+    let cityName = '';
+    if (this.state.weather !== '') {
+      weather = Math.round(this.state.weather.main.temp - 273.15);
+      weatherType = this.state.weather.weather[0].main;
+      cityName = this.state.weather.name;
+    }
     return (
       <div className="page">
-        <header className="header">
+        <header className="header sticky">
+          <div className="App weather">Weather in {this.renderSearch(cityName)}: {weather}º, {weatherType}</div>
           <button className="App headerTabs" onClick={this.props.scrollToAbout}>About</button>
           <button className="App headerTabs" onClick={this.props.scrollToProjects}>Projects</button>
           <button className="App headerTabs" onClick={this.props.scrollToExperiences}>Experience</button>
